314a1e46373d0b68f80df8e0ab382e991210e288
pplCRM <hello@pplcrm.com>
2023-12-09 22:52:11 -0500

    Shared common types.

--- apps/frontend/src/app/login/login.component.ts
+++ apps/frontend/src/app/login/login.component.ts
@@ -10,29 +10,9 @@ import {
 } from "@angular/forms";
 
 import { Router } from "@angular/router";
+import * as common from "@common";
 import { ToastrService } from "ngx-toastr";
 import { AuthService } from "../services/auth.service.js";
-
-interface IAuthUser {
-  // #region Properties (3)
-
-  error: AuthErrors | null;
-  session: unknown | null;
-  user: unknown | null;
-
-  // #endregion Properties (3)
-}
-
-enum AuthErrors {
-  BadLogin = 1,
-  EmailNotConfirmed,
-  InvalidRefreshToken,
-  AdminTokenRequired,
-  MissingInformation,
-  UserAlreadyRegistered,
-  BadPassword,
-  Unknown,
-}
 
 @Component({
   selector: "pplcrm-login",
@@ -79,16 +59,16 @@ export class LoginComponent {
 
   // #region Public Methods (2)
 
-  public mapErrorToString(error?: AuthErrors) {
+  public mapErrorToString(error?: common.AuthErrors) {
     switch (error) {
-      case AuthErrors.InvalidRefreshToken:
-      case AuthErrors.AdminTokenRequired:
+      case common.AuthErrors.InvalidRefreshToken:
+      case common.AuthErrors.AdminTokenRequired:
         return "There was an error logging you in. Please try again.";
-      case AuthErrors.BadPassword:
-      case AuthErrors.MissingInformation:
+      case common.AuthErrors.BadPassword:
+      case common.AuthErrors.MissingInformation:
         return "Please check your email and password";
-      case AuthErrors.BadLogin:
-      case AuthErrors.Unknown:
+      case common.AuthErrors.BadLogin:
+      case common.AuthErrors.Unknown:
       default:
         return "Sorry, could not log you in. Please check your network connection, email and password. If the issue persists then contact us.";
     }
@@ -96,14 +76,14 @@ export class LoginComponent {
 
   public async signIn() {
     if (!(this.email?.valid && this.password?.valid)) {
-      this.toastr.error(this.mapErrorToString(AuthErrors.BadPassword));
+      this.toastr.error(this.mapErrorToString(common.AuthErrors.BadPassword));
       return;
     }
 
     this.toastr.clear();
     this.processing.set(true);
 
-    const payload: Partial<IAuthUser> = await this.authService.signIn({
+    const payload: Partial<common.IAuthUser> = await this.authService.signIn({
       email: this.email.value as string,
       password: this.password.value as string,
     });
@@ -111,7 +91,7 @@ export class LoginComponent {
     console.log("payload", payload);
 
     if (payload?.error) {
-      if (payload?.error === AuthErrors.EmailNotConfirmed) {
+      if (payload?.error === common.AuthErrors.EmailNotConfirmed) {
         // TODO: continue to the 'verify email' component
       }
       this.toastr.error(this.mapErrorToString(payload.error));
