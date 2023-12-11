f65071be5e6b5c80dc11d62a2b26fda6a8d1a18a
pplCRM <hello@pplcrm.com>
2023-12-10 21:21:33 -0500

    Some code cleanup.

--- apps/backend/src/app/trpc.routers/households.router.ts
+++ apps/backend/src/app/trpc.routers/households.router.ts
@@ -6,18 +6,6 @@ const publicProcedure = trpc.procedure;
 const router = trpc.router;
 const operator = new HouseholdOperator();
 
-/*
-export interface ITodoBody {
-  todo: string;
-  done: boolean;
-}
-
-export interface ITodo extends Partial<ITodoBody> {
-  id: number;
-}
-
-*/
-
 export const householdsRouter = router({
   getOneById: publicProcedure.input(z.number()).query((input) => {
     const id = input;
@@ -26,40 +14,6 @@ export const householdsRouter = router({
   getAll: publicProcedure.query(() => {
     return operator.getAll();
   }),
-  /*
-  add: publicProcedure
-    .input(
-      z.object({
-        todo: z.string(),
-        done: z.boolean(),
-      })
-    )
-    .mutation((input) => {
-      const newTodo: ITodo = {
-        id: ++id,
-        ...input,
-      };
-      todos.push(newTodo);
-      return newTodo;
-    }),
-  update: publicProcedure
-    .input(
-      z.object({
-        id: z.number(),
-        todo: z.string().optional(),
-        done: z.boolean().optional(),
-      })
-    )
-    .mutation(({ input }) => {
-      todos = todos.map((t: ITodo) => (t.id === input.id ? (input as ITodo) : t));
-      return input as ITodo;
-    }),
-  delete: publicProcedure.input(z.number()).mutation(({ input }): ITodo => {
-    const todoToDelete: ITodo = todos.find((todo) => todo.id === input) as ITodo;
-    todos = todos.filter((todo) => todo.id !== input);
-    return todoToDelete;
-  }),
-  */
 });
 
 export type HouseholdsRouter = typeof householdsRouter;
