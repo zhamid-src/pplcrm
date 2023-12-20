3e420c71a23bd9c5bf61be0446e48555836e1f2a
zhamid <zhamid@gmail.com>
2023-12-19 20:20:34 -0500

    Adding swap component

--- apps/frontend/src/app/navbar/navbar.component.ts
+++ apps/frontend/src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+
 import { FormsModule } from "@angular/forms";
 import { IconsComponent } from "../icons/icons.component";
 import { AuthService } from "../services/auth.service";
 import { SearchService } from "../services/search.service";
 import { ThemeService } from "../services/theme.service";
+import { SwapComponent } from "../swap/swap.component";
 
 @Component({
   selector: "pplcrm-navbar",
   standalone: true,
-  imports: [CommonModule, IconsComponent, FormsModule],
+  imports: [CommonModule, IconsComponent, SwapComponent, FormsModule],
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.scss",
 })
---
+++ apps/frontend/src/app/swap/swap.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { IconsComponent } from "../icons/icons.component";
+
+@Component({
+  selector: "pplcrm-swap",
+  standalone: true,
+  imports: [CommonModule, FormsModule, IconsComponent],
+  templateUrl: "./swap.component.html",
+  styleUrl: "./swap.component.scss",
+})
+export class SwapComponent {
+  @Input({ required: true }) public swapOnIcon: string | undefined;
+  @Input({ required: true }) public swapOffIcon: string | undefined;
+
+  @Input() checked: boolean = false;
+  @Output() clickEvent = new EventEmitter();
+
+  emitClick() {
+    this.clickEvent.emit();
+  }
+}
