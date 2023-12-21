99b8617eb4e162e00bb9ed7f4091e8d74cecd6a0
zhamid <zhamid@gmail.com>
2023-12-21 08:04:33 -0500

    simple cache in personss.service.ts

--- apps/frontend/src/app/services/persons.service.ts
+++ apps/frontend/src/app/services/persons.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from "@angular/core";
 import { getAllOptionsType } from "@common";
-import { from } from "rxjs";
+import { TableType } from "common/src/lib/kysely.models";
+import { Observable, from } from "rxjs";
 import { TRPCService } from "./trpc.service";
+
+export type TYPE = TableType.persons | TableType.households;
 
 @Injectable({
   providedIn: "root",
 })
 export class PersonsService extends TRPCService {
-  public getAllWithHouseholds(options?: getAllOptionsType) {
-    return from(this.api.persons.getAllWithHouseholds.query(options));
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private getAllCache: Record<string, Observable<Partial<TYPE>[]>> = {};
+
+  public getAllWithHouseholds(
+    options?: getAllOptionsType,
+    refresh: boolean = false,
+  ) {
+    const cacheKeyJSON = {
+      query: "getAllWithHouseholds",
+      ...options,
+    };
+    const cacheKey = JSON.stringify(cacheKeyJSON);
+    if (refresh || !this.getAllCache[cacheKey]) {
+      this.getAllCache[cacheKey] = from(
+        this.api.persons.getAllWithHouseholds.query(options),
+      );
+    }
+    return this.getAllCache[cacheKey];
   }
 
   public getOneById(id: number) {
