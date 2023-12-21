9407304a6d654054e80f5a6e8358f679f686dd46
zhamid <zhamid@gmail.com>
2023-12-21 00:56:43 -0500

    creating a datagrid component

--- apps/frontend/src/app/components/people/people.component.ts
+++ apps/frontend/src/app/components/people/people.component.ts
@@ -1,16 +1,8 @@
 import { CommonModule } from "@angular/common";
-import { Component, effect } from "@angular/core";
+import { Component } from "@angular/core";
 import { PersonsService } from "@services/persons.service";
-import { SearchService } from "@services/search.service";
-import { ThemeService } from "@services/theme.service";
-import { IconsComponent } from "@uxcommon/icons/icons.component";
-import { AgGridModule } from "ag-grid-angular";
-import {
-  ColDef,
-  GridApi,
-  GridOptions,
-  GridReadyEvent,
-} from "ag-grid-community";
+import { DatagridComponent } from "@uxcommon/datagrid/datagrid.component";
+import { ColDef } from "ag-grid-community";
 import { TableType } from "common/src/lib/kysely.models";
 
 type TYPE = TableType.persons | TableType.households;
@@ -18,15 +10,17 @@ type TYPE = TableType.persons | TableType.households;
 @Component({
   selector: "pplcrm-people",
   standalone: true,
-  imports: [CommonModule, AgGridModule, IconsComponent],
+  imports: [CommonModule, DatagridComponent],
   templateUrl: "./people.component.html",
   styleUrl: "./people.component.scss",
 })
 export class PeopleComponent {
-  private api: GridApi<Partial<TYPE>> | undefined;
-
   protected colDefs: ColDef[] = [
-    { field: "first_name", headerName: "First Name", checkboxSelection: true },
+    {
+      field: "first_name",
+      headerName: "First Name",
+      checkboxSelection: true,
+    },
     { field: "last_name", headerName: "Last Name" },
     { field: "email", headerName: "Email" },
     { field: "mobile", headerName: "Mobile" },
@@ -34,58 +28,13 @@ export class PeopleComponent {
     { field: "city", headerName: "City" },
     { field: "notes", headerName: "Notes" },
   ];
-  protected defaultColDef: ColDef = {
-    // filter: true,
-  };
+
   protected rowData: Partial<TYPE>[] = [];
-  protected rowSelection: "single" | "multiple" = "multiple";
 
-  public _loading =
-    '<span class="ag-overlay-loading-center">Download data ... <span class="inline loading loading-infinity"></span></span>';
-  public gridOptions: GridOptions<Partial<TYPE>> = {
-    rowStyle: { cursor: "pointer" },
-    suppressCellFocus: true,
-    overlayLoadingTemplate: this._loading,
-    onRowSelected: this.onRowSelected.bind(this),
-    // other grid options ...
-  };
+  constructor(private personsSvc: PersonsService) {}
 
-  constructor(
-    private personsSvc: PersonsService,
-    private themeSvc: ThemeService,
-    private serachSvc: SearchService,
-  ) {
-    // Set effects
-    effect(() => {
-      const quickFilterText = this.serachSvc.search;
-      this.api?.updateGridOptions({ quickFilterText });
-    });
-  }
-
-  public exportToCSV() {
-    this.api!.exportDataAsCsv();
-  }
-
-  public getTheme() {
-    return this.themeSvc.theme === "light"
-      ? "ag-theme-quartz"
-      : "ag-theme-quartz-dark";
-  }
-
-  public onGridReady(params: GridReadyEvent) {
-    this.api = params.api;
-    this.refreshGrid();
-  }
-
-  public onRowSelected(/*event: RowSelectedEvent<Partial<TYPE>>*/) {
-    // const selectedRow = event.data;
-    // const selectedRows = this.api!.getSelectedRows();
-  }
-
-  public refreshGrid() {
-    this.api!.showLoadingOverlay();
-
-    this.personsSvc
+  public refresh() {
+    return this.personsSvc
       .getAllWithHouseholds()
       .subscribe((data) => (this.rowData = data));
   }
---
+++ apps/frontend/src/app/uxcommon/datagrid/datagrid.component.ts
@@ -0,0 +1,92 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output, effect } from "@angular/core";
+import { SearchService } from "@services/search.service";
+import { ThemeService } from "@services/theme.service";
+import { IconsComponent } from "@uxcommon/icons/icons.component";
+import { AgGridModule } from "ag-grid-angular";
+import {
+  ColDef,
+  GridApi,
+  GridOptions,
+  GridReadyEvent,
+  RowSelectedEvent,
+} from "ag-grid-community";
+
+@Component({
+  selector: "pplcrm-datagrid",
+  standalone: true,
+  imports: [CommonModule, AgGridModule, IconsComponent],
+  templateUrl: "./datagrid.component.html",
+  styleUrl: "./datagrid.component.scss",
+})
+export class DatagridComponent<T> {
+  protected api: GridApi<Partial<T>> | undefined;
+
+  @Input({ required: true }) colDefs: ColDef[] = [];
+  @Input({ required: true }) rowData: Partial<T>[] = [];
+  @Input() gridOptions: GridOptions<Partial<T>> = {};
+
+  @Output() refresh = new EventEmitter<void>();
+
+  public _loading =
+    '<span class="ag-overlay-loading-center">Download data ... <span class="inline loading loading-infinity"></span></span>';
+
+  defaultGridOptions: GridOptions<Partial<T>> = {
+    rowStyle: { cursor: "pointer" },
+    suppressCellFocus: true,
+    overlayLoadingTemplate: this._loading,
+    pagination: true,
+    paginationAutoPageSize: true,
+    rowSelection: "multiple",
+    onRowSelected: this.onRowSelected.bind(this),
+    onRowDataUpdated: this.onRowDataUpdated.bind(this),
+  };
+
+  protected combinedGridOptions: GridOptions<Partial<T>> = {
+    ...this.defaultGridOptions,
+    ...this.gridOptions,
+  };
+
+  constructor(
+    private themeSvc: ThemeService,
+    private serachSvc: SearchService,
+  ) {
+    // Set effects
+    effect(() => {
+      const quickFilterText = this.serachSvc.search;
+      this.api?.updateGridOptions({ quickFilterText });
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public onRowDataUpdated(event: any) {
+    console.log("onRowDataUpdated", event);
+  }
+
+  public exportToCSV() {
+    this.api!.exportDataAsCsv();
+  }
+
+  public getTheme() {
+    return this.themeSvc.theme === "light"
+      ? "ag-theme-quartz"
+      : "ag-theme-quartz-dark";
+  }
+
+  public onGridReady(params: GridReadyEvent) {
+    this.api = params.api;
+    this.refreshGrid();
+  }
+
+  public onRowSelected(event: RowSelectedEvent<Partial<T>>) {
+    const selectedRow = event.data;
+    console.log(selectedRow);
+    // const selectedRows = this.api!.getSelectedRows();
+  }
+
+  public refreshGrid() {
+    this.api!.showLoadingOverlay();
+
+    this.refresh.emit();
+  }
+}
