43bb93739d3835856169c05778c2382fb6b9549d
pplCRM <hello@pplcrm.com>
2023-12-23 02:47:24 -0500

    Undoing accidental commit of the summary component

--- apps/frontend/src/app/components/summary/summary.component.ts
+++ apps/frontend/src/app/components/summary/summary.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { PplCrmToastrService } from "@services/pplcrm-toast.service";
 
 @Component({
   selector: "pc-summary",
@@ -9,23 +8,4 @@ import { PplCrmToastrService } from "@services/pplcrm-toast.service";
   templateUrl: "./summary.component.html",
   styleUrl: "./summary.component.scss",
 })
-export class SummaryComponent {
-  constructor(private readonly _toastService: PplCrmToastrService) {}
-  openToast(type: string) {
-    console.log("openToast", type);
-    switch (type) {
-      case "info":
-        this._toastService.info("Hello world");
-        break;
-      case "success":
-        this._toastService.success("Hello world s");
-        break;
-      case "warning":
-        this._toastService.warn("Hello world w");
-        break;
-      case "error":
-        this._toastService.error("Hello world e");
-        break;
-    }
-  }
-}
+export class SummaryComponent {}
