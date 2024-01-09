8ed574aeb66762400aea7d95555b4bef818ac344
zhamid <zhamid@gmail.com>
2024-01-08 23:01:01 -0500

    Using a better secret key and algorithm for jwt

--- apps/backend/src/app/kyselyinit.ts
+++ apps/backend/src/app/kyselyinit.ts
@@ -1,14 +1,9 @@
-import { config } from "dotenv";
-import { promises as fs } from "fs";
-import {
-  FileMigrationProvider,
-  Kysely,
-  Migrator,
-  PostgresDialect,
-} from "kysely";
-import path from "path";
-import { Pool } from "pg";
-import { Models } from "../../../../common/src/lib/kysely.models";
+import { config } from 'dotenv';
+import { promises as fs } from 'fs';
+import { FileMigrationProvider, Kysely, Migrator, PostgresDialect } from 'kysely';
+import path from 'path';
+import { Pool } from 'pg';
+import { Models } from '../../../../common/src/lib/kysely.models';
 
 // import configs from env via dotenv
 config();
@@ -16,11 +11,11 @@ config();
 const dialect = new PostgresDialect({
   pool: new Pool({
     // connectionString: process.env["DATABASE_URL"],
-    user: "zeehamid",
-    database: "pplcrm",
-    password: "Eternity#1",
+    user: 'zeehamid',
+    database: 'pplcrm',
+    password: 'Eternity#1',
     port: 5432,
-    host: "localhost",
+    host: 'localhost',
     ssl: false,
   }),
 });
@@ -31,7 +26,7 @@ export const db = new Kysely<Models>({
 });
 
 // Run the migrations
-const migrationFolder = path.join(__dirname, "./kysely.migrations");
+const migrationFolder = path.join(__dirname, './kysely.migrations');
 export const migrator = new Migrator({
   db,
   provider: new FileMigrationProvider({
@@ -46,15 +41,15 @@ async function migrateToLatest() {
   const { error, results } = await migrator.migrateToLatest();
 
   results?.forEach((it) => {
-    if (it.status === "Success") {
+    if (it.status === 'Success') {
       console.log(`migration up:"${it.migrationName}" successful`);
-    } else if (it.status === "Error") {
+    } else if (it.status === 'Error') {
       console.error(`failed to execute migration up"${it.migrationName}"`);
     }
   });
 
   if (error) {
-    console.error("failed to migrate up: ", error);
+    console.error('failed to migrate up: ', error);
     process.exit(1);
   }
 }
@@ -66,15 +61,15 @@ export async function migrateDown() {
   const { error, results } = await migrator.migrateDown();
 
   results?.forEach((it) => {
-    if (it.status === "Success") {
+    if (it.status === 'Success') {
       console.log(`migration down"${it.migrationName}" successsful`);
-    } else if (it.status === "Error") {
+    } else if (it.status === 'Error') {
       console.error(`failed to execute migration down"${it.migrationName}"`);
     }
   });
 
   if (error) {
-    console.error("failed to migrate down: ", error);
+    console.error('failed to migrate down: ', error);
     process.exit(1);
   }
 }
