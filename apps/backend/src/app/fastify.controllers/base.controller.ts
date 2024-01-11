d6ea3fdbe5ca063c258243641072018fe4de95ff
zhamid <zhamid@gmail.com>
2024-01-11 08:09:04 -0500

    Removing fastifyreply

--- apps/backend/src/app/fastify.controllers/base.controller.ts
+++ apps/backend/src/app/fastify.controllers/base.controller.ts
@@ -1,4 +1,3 @@
-import { FastifyReply } from 'fastify';
 import {
   Models,
   OperationDataType,
@@ -16,27 +15,27 @@ export class BaseController<T extends keyof Models> {
     this.operator = dbOperator;
   }
 
-  public async add(row: OperationDataType<T, 'insert'>, reply: FastifyReply) {
+  public async add(row: OperationDataType<T, 'insert'>) {
     return this.operator.addOne(row);
   }
 
-  public delete(id: TableIdType<T>, reply: FastifyReply) {
+  public delete(id: TableIdType<T>) {
     return this.operator.deleteOne(id);
   }
 
-  public findOne(id: TableIdType<T>, reply: FastifyReply) {
+  public findOne(id: TableIdType<T>) {
     return this.operator.findOne(id);
   }
 
-  public getAll(reply: FastifyReply) {
+  public getAll() {
     return this.operator.findAll();
   }
 
-  public async getCount(reply: FastifyReply) {
+  public async getCount() {
     return this.operator.getCount();
   }
 
-  public update(id: TableIdType<T>, row: OperationDataType<T, 'update'>, reply: FastifyReply) {
+  public update(id: TableIdType<T>, row: OperationDataType<T, 'update'>) {
     return this.operator.updateOne(id, row);
   }
 }
