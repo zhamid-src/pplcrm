fa68a79d804e0e650c3507042e0a1cf024d0ea2c
pplCRM <hello@pplcrm.com>
2024-01-12 14:32:05 -0500

    renaming add-tag

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 import { NotFoundComponent } from '@uxcommon/not-found/not-found.component';
-import { AddTagComponent } from '@uxcommon/tags/add/addTag.component';
+import { AddTagComponent } from '@uxcommon/tags/add/add-tag.component';
 import { NewPasswordComponent } from './auth/new-password/new-password.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 import { SignInComponent } from './auth/signin/signin.component';
