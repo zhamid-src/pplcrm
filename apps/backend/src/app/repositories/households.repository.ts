31f1dcbd988fd05c31059da836af833e895e46f0
pplCRM <hello@pplcrm.com>
2024-01-23 09:11:11 -0500

    moving household tags

--- apps/backend/src/app/repositories/households.repository.ts
+++ apps/backend/src/app/repositories/households.repository.ts
@@ -18,4 +18,23 @@ export class HouseholdRepo extends BaseRepository<'households'> {
       .groupBy(['households.id', 'households.tenant_id'])
       .execute();
   }
+  public getDistinctTags(tenant_id: bigint) {
+    return this.getSelect()
+      .innerJoin('map_households_tags', 'map_households_tags.household_id', 'households.id')
+      .innerJoin('tags', 'tags.id', 'map_households_tags.tag_id')
+      .where('households.tenant_id', '=', tenant_id)
+      .select('tags.name')
+      .distinct()
+      .execute();
+  }
+
+  public getTags(id: bigint, tenant_id: bigint) {
+    return this.getSelect()
+      .innerJoin('map_households_tags', 'map_households_tags.household_id', 'households.id')
+      .innerJoin('tags', 'tags.id', 'map_households_tags.tag_id')
+      .where('households.id', '=', id)
+      .where('households.tenant_id', '=', tenant_id)
+      .select('tags.name')
+      .execute();
+  }
 }
