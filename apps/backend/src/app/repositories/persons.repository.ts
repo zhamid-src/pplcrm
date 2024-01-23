16e4fb63e657f57cf83a1d49225106f8603d7309
pplCRM <hello@pplcrm.com>
2024-01-23 09:07:48 -0500

    moving stuff to the persons repo

--- apps/backend/src/app/repositories/persons.repository.ts
+++ apps/backend/src/app/repositories/persons.repository.ts
@@ -4,4 +4,22 @@ export class PersonsRepository extends BaseRepository<'persons'> {
   constructor() {
     super('persons');
   }
+  public getTags(id: bigint, tenant_id: bigint) {
+    return this.getSelect()
+      .innerJoin('map_peoples_tags', 'map_peoples_tags.person_id', 'persons.id')
+      .innerJoin('tags', 'tags.id', 'map_peoples_tags.tag_id')
+      .where('persons.id', '=', id)
+      .where('persons.tenant_id', '=', tenant_id)
+      .select('tags.name')
+      .execute();
+  }
+  public getDistinctTags(tenant_id: bigint) {
+    return this.getSelect()
+      .innerJoin('map_peoples_tags', 'map_peoples_tags.person_id', 'persons.id')
+      .innerJoin('tags', 'tags.id', 'map_peoples_tags.tag_id')
+      .where('persons.tenant_id', '=', tenant_id)
+      .select('tags.name')
+      .distinct()
+      .execute();
+  }
 }
