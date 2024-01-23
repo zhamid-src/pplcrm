e7466ca26bb4b62c8a80b46a316b8e56a716e98f
pplCRM <hello@pplcrm.com>
2024-01-22 20:41:07 -0500

    ...

--- apps/backend/src/app/trpc-routers/households.router.ts
+++ apps/backend/src/app/trpc-routers/households.router.ts
@@ -4,12 +4,35 @@ import { HouseholdsController } from '../controllers/households.controller';
 
 const households = new HouseholdsController();
 
+/**
+ * Household endpoints
+ */
 export const HouseholdsRouter = router({
-  getById: authProcedure.input(z.string()).query(({ input }) => households.getById(BigInt(input))),
-  getAll: authProcedure.query(() => households.getAll()),
-  getAllWithPeopleCount: authProcedure.query(() => households.getAllWithPeopleCount()),
-  getTags: authProcedure
-    .input(z.string())
-    .query(({ input, ctx }) => households.getTags(BigInt(input), ctx.auth!)),
-  getDistinctTags: authProcedure.query(({ ctx }) => households.getDistinctTags(ctx.auth!)),
+  getAll: getAll(),
+  getById: getById(),
+  getTags: getTags(),
+  getDistinctTags: getDistinctTags(),
+  getAllWithPeopleCount: getAllWithPeopleCount(),
 });
+
+function getDistinctTags() {
+  return authProcedure.query(({ ctx }) => households.getDistinctTags(ctx.auth!));
+}
+
+function getAllWithPeopleCount() {
+  return authProcedure.query(() => households.getAllWithPeopleCount());
+}
+
+function getTags() {
+  return authProcedure
+    .input(z.string())
+    .query(({ input, ctx }) => households.getTags(BigInt(input), ctx.auth!));
+}
+
+function getAll() {
+  return authProcedure.query(() => households.getAll());
+}
+
+function getById() {
+  return authProcedure.input(z.string()).query(({ input }) => households.getById(BigInt(input)));
+}
--- apps/backend/src/app/trpc-routers/tags.router.ts
+++ apps/backend/src/app/trpc-routers/tags.router.ts
@@ -5,16 +5,46 @@ import { TagsController } from '../controllers/tags.controller';
 
 const tags = new TagsController();
 
+/**
+ * Tags endpoints
+ */
 export const TagsRouter = router({
-  getByName: authProcedure.input(z.string()).query(({ input }) => tags.getById(BigInt(input))),
-  getById: authProcedure.input(z.string()).query(({ input }) => tags.getById(BigInt(input))),
-  getAll: authProcedure.query(() => tags.getAll()),
-  add: authProcedure.input(AddTagObj).mutation(({ input, ctx }) => tags.addTag(input, ctx.auth!)),
-  delete: authProcedure.input(z.string()).mutation(({ input }) => tags.delete(BigInt(input))),
-  update: authProcedure
-    .input(z.object({ id: z.string(), data: UpdateTagObj }))
-    .mutation(({ input, ctx }) => tags.updateTag(BigInt(input.id), input.data, ctx.auth!)),
-  findByName: authProcedure
-    .input(z.string())
-    .query(({ input, ctx }) => tags.findByName(input, ctx.auth!)),
+  add: add(),
+  getAll: getAll(),
+  delete: deleteTag(),
+  update: update(),
+  getById: getById(),
+  getByName: getByName(),
+  findByName: findByName(),
 });
+function findByName() {
+  return authProcedure
+    .input(z.string())
+    .query(({ input, ctx }) => tags.findByName(input, ctx.auth!));
+}
+
+function update() {
+  return authProcedure
+    .input(z.object({ id: z.string(), data: UpdateTagObj }))
+    .mutation(({ input, ctx }) => tags.updateTag(BigInt(input.id), input.data, ctx.auth!));
+}
+
+function deleteTag() {
+  return authProcedure.input(z.string()).mutation(({ input }) => tags.delete(BigInt(input)));
+}
+
+function add() {
+  return authProcedure.input(AddTagObj).mutation(({ input, ctx }) => tags.addTag(input, ctx.auth!));
+}
+
+function getAll() {
+  return authProcedure.query(() => tags.getAll());
+}
+
+function getById() {
+  return authProcedure.input(z.string()).query(({ input }) => tags.getById(BigInt(input)));
+}
+
+function getByName() {
+  return authProcedure.input(z.string()).query(({ input }) => tags.getById(BigInt(input)));
+}
