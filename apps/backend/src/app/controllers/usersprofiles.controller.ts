5ed0c629d825ae1291728ab2822d25133dbe0712
zhamid <zhamid@gmail.com>
2024-01-23 09:18:50 -0500

    renaming userprofiles

--- apps/backend/src/app/controllers/usersprofiles.controller.ts
+++ apps/backend/src/app/controllers/usersprofiles.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { UserPofiles } from '../repositories/user-profiles.repository';
+import { UserPofiles } from '../repositories/userprofiles.repository';
 import { BaseController } from './base.controller';
 
 export class UserProfilesController extends BaseController<'profiles', UserPofiles> {
