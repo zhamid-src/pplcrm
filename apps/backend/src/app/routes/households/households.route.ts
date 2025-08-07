73e568b0d8a48678cafef9c29c1b6f99a7526c02
zhamid-src <52141733+zhamid-src@users.noreply.github.com>
2025-08-07 19:42:18 -0400

    feat: add REST get endpoints

--- apps/backend/src/app/routes/households/households.route.ts
+++ apps/backend/src/app/routes/households/households.route.ts
@@ -1,18 +1,24 @@
-import { FastifyPluginCallback } from 'fastify';
+import { FastifyPluginCallback, FastifyRequest } from 'fastify';
 
-// const households = new HouseholdsController();
+import { HouseholdsController } from '../../controllers/households.controller';
+import * as schema from './households.schema';
+import { IdParam } from '../fastify.types';
+
+const households = new HouseholdsController();
 
 /**
  * Supported HTTP routes for the households endpoint
  */
-const routes: FastifyPluginCallback = (_fastify, _, done) => {
-  /*
-  fastify.get('', schema.getAll, () => households.getAll());
-
-  fastify.get('/:id', schema.findFromId, (req: IdParam) => households.getById(req.params.id));
-  fastify.get('/count', schema.count, () => households.getCount());
-  fastify.delete('/:id', schema.findFromId, (req: IdParam) => households.delete(req.params.id));
-  */
+const routes: FastifyPluginCallback = (fastify, _, done) => {
+  fastify.get('', schema.getAll, (req: FastifyRequest) =>
+    households.getAll(req.headers['tenant-id'] as string),
+  );
+  fastify.get('/:id', schema.findFromId, (req: IdParam) =>
+    households.getById({ tenant_id: req.headers['tenant-id'] as string, id: req.params.id }),
+  );
+  fastify.get('/count', schema.count, (req: FastifyRequest) =>
+    households.getCount(req.headers['tenant-id'] as string),
+  );
 
   done();
 };
--- apps/backend/src/app/routes/persons/persons.route.ts
+++ apps/backend/src/app/routes/persons/persons.route.ts
@@ -1,24 +1,25 @@
-import { FastifyPluginCallback } from 'fastify';
+import { FastifyPluginCallback, FastifyRequest } from 'fastify';
 
-//const persons = new PersonsController();
+import { PersonsController } from '../../controllers/persons.controller';
+import * as schema from './persons.schema';
+import { IdParam } from '../fastify.types';
+
+const persons = new PersonsController();
 
 /**
  * Supported HTTP routes for the persons endpoint
  */
-const routes: FastifyPluginCallback = (_fastify, _, done) => {
-  /*
-  fastify.get('', schema.getAll, () => persons.getAll());
+const routes: FastifyPluginCallback = (fastify, _, done) => {
+  fastify.get('', schema.getAll, (req: FastifyRequest) =>
+    persons.getAll(req.headers['tenant-id'] as string),
+  );
+  fastify.get('/:id', schema.findFromId, (req: IdParam) =>
+    persons.getById({ tenant_id: req.headers['tenant-id'] as string, id: req.params.id }),
+  );
+  fastify.get('/count', schema.count, (req: FastifyRequest) =>
+    persons.getCount(req.headers['tenant-id'] as string),
+  );
 
-  fastify.get('/:id', schema.findFromId, (req: IdParam) => persons.getById(req.params.id));
-  fastify.get('/count', schema.count, () => persons.getCount());
-  fastify.post('', schema.update, (req) =>
-    persons.add(req.body as OperationDataType<'persons', 'insert'>),
-  );
-  fastify.patch('/:id', schema.findFromId, (req: IdParam) =>
-    persons.update(req.params.id, req.body as OperationDataType<'persons', 'insert'>),
-  );
-  fastify.delete('/:id', schema.findFromId, (req: IdParam) => persons.delete(req.params.id));
-*/
   done();
 };
 
