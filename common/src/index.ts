7f4048d6903b1e0f2a6fd3379c16fb7ac0695d04
pplCRM <hello@pplcrm.com>
2025-08-07 19:22:24 -0400

    refactor(common): organize lib into submodules

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,3 +1,35 @@
-export * from './lib/common';
+export {
+  IAuthKeyPayload,
+  IAuthUser,
+  IToken,
+  signInInputObj,
+  signInInputType,
+  signUpInputObj,
+  signUpInputType,
+} from './lib/auth';
 
-export * from './lib/utils';
+export {
+  INow,
+  AddTagType,
+  PERSONINHOUSEHOLDTYPE,
+  PersonsType,
+  SettingsType,
+  SortModelType,
+  UpdateHouseholdsType,
+  UpdatePersonsType,
+  UpdateTagType,
+  getAllOptionsType,
+} from './lib/models';
+
+export {
+  AddTagObj,
+  PersonsObj,
+  SettingsObj,
+  UpdateHouseholdsObj,
+  UpdatePersonsObj,
+  UpdateTagObj,
+  sortModelItem,
+  getAllOptions,
+} from './lib/schemas';
+
+export { debounce, sleep } from './lib/utils';
