f4e848821cd99deff7f3bd383dc38eecd3fae63e
pplCRM <hello@pplcrm.com>
2025-08-09 12:48:06 -0400

    refactor: replace deprecated setup-jest

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
