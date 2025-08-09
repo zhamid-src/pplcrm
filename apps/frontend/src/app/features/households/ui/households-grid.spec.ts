fe3d2499cce554083d6e31e4dd8bba0f807e3ffe
pplCRM <hello@pplcrm.com>
2025-08-09 10:13:59 -0400

    test: add coverage for grids and services

---
+++ apps/frontend/src/app/features/households/ui/households-grid.spec.ts
@@ -0,0 +1,31 @@
+jest.mock('@uxcommon/datagrid/datagrid', () => {
+  return {
+    DataGrid: class {
+      protected tagArrayEquals() {
+        return 0;
+      }
+      protected tagsToString(tags: string[]) {
+        return tags?.toString() ?? '';
+      }
+      protected openEditOnDoubleClick() {}
+    },
+  };
+});
+
+jest.mock('../../tags/ui/tags-cell-renderer', () => ({ TagsCellRenderer: class {} }), { virtual: true });
+
+import { HouseholdsGrid } from './households-grid';
+
+describe('HouseholdsGrid', () => {
+  let component: HouseholdsGrid;
+
+  beforeEach(() => {
+    component = new HouseholdsGrid();
+  });
+
+  it('should define persons_count column', () => {
+    const hasPersonsCount = component['col'].some((c) => c.field === 'persons_count');
+    expect(hasPersonsCount).toBe(true);
+  });
+});
+
