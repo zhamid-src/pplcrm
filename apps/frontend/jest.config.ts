72f1f40b7d168fcec69e6509ce459b6a4a0b12b6
pplCRM <hello@pplcrm.com>
2025-08-10 08:18:55 -0400

    refactor: load icons from asset files

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -12,11 +12,13 @@ const config: Config = {
   moduleNameMapper: {
     '^common/(.*)$': '<rootDir>/../../common/$1',
     '^@uxcommon/(.*)$': '<rootDir>/src/app/uxcommon/$1',
+    '^@icons/(.*)$': '<rootDir>/src/app/uxcommon/icons/$1',
     '^@services/(.*)$': '<rootDir>/src/app/services/$1',
     '^@pipes/(.*)$': '<rootDir>/src/app/pipes/$1',
     '^@common$': '<rootDir>/../../common/src/index.ts',
     '^apps/frontend/(.*)$': '<rootDir>/$1',
-    '\\.(html)$': '<rootDir>/src/__mocks__/html.mock.ts'
+    '\\.(html)$': '<rootDir>/src/__mocks__/html.mock.ts',
+    '\\.svg\\?raw$': '<rootDir>/src/__mocks__/svg.mock.ts'
   },
   moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
