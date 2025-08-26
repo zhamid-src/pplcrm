1b4b27ea9451881c41c8e168251781f4e2019dc0
pplCRM <hello@pplcrm.com>
2025-08-26 06:28:29 -0400

    chore: declare import meta env

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pplcrm.example.com',
-  googleMapsApiKey: process.env['NX_GOOGLE_MAPS_API_KEY'] ?? '',
+  googleMapsApiKey: import.meta.env['NX_GOOGLE_MAPS_API_KEY'] ?? '',
 };
