8318eb616ba572d6e645a34457f05962e76964f7
pplCRM <hello@pplcrm.com>
2025-09-14 19:56:42 -0400

    cleaning up grid

--- apps/frontend/src/app/uxcommon/components/datagrid/ui/inline-filters-row.ts
+++ apps/frontend/src/app/uxcommon/components/datagrid/ui/inline-filters-row.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, inject } from '@angular/core';
 import type { ColumnDef as ColDef } from '../grid-defaults';
+import { DataGrid } from '../datagrid';
 
 @Component({
   selector: 'pc-dg-inline-filters-row',
@@ -50,16 +51,20 @@ import type { ColumnDef as ColDef } from '../grid-defaults';
   `,
 })
 export class DataGridInlineFiltersRowComponent {
+  private readonly grid = inject<DataGrid<any, any>>(DataGrid as any, { optional: true });
+
   enableSelection = input<boolean>(true);
   selectionStickyWidth = input<number>(48);
   leafHeaders = input<any[]>([]);
 
-  getColDefById = input<(id: string) => ColDef | undefined>((_id) => undefined);
-  getFilterOptionsForCol = input<(col: ColDef) => string[] | null>((_c) => null);
-  inlineFilterLabel = input<(field: string) => string>((_f) => '');
-  isOptionChecked = input<(field: string, option: string) => boolean>((_f, _o) => false);
-  onToggleFilterOption = input<(field: string, option: string, checked: boolean) => void>((_f, _o, _c) => {});
-  onHeaderFilterInput = input<(field: string, value: any) => void>((_f, _v) => {});
-  clearHeaderFilter = input<(field: string) => void>((_f) => {});
-  getFilterValue = input<(field: string) => string>((_f) => '');
+  getColDefById = input<(id: string) => ColDef | undefined>((id) => this.grid?.getColDefById(id));
+  getFilterOptionsForCol = input<(col: ColDef) => string[] | null>((c) => this.grid?.getFilterOptionsForCol(c) ?? null);
+  inlineFilterLabel = input<(field: string) => string>((f) => this.grid?.inlineFilterLabel?.(f) ?? '');
+  isOptionChecked = input<(field: string, option: string) => boolean>((f, o) => !!this.grid?.isOptionChecked(f, o));
+  onToggleFilterOption = input<(field: string, option: string, checked: boolean) => void>((f, o, c) =>
+    this.grid?.onToggleFilterOption(f, o, c),
+  );
+  onHeaderFilterInput = input<(field: string, value: any) => void>((f, v) => this.grid?.onHeaderFilterInput(f, v));
+  clearHeaderFilter = input<(field: string) => void>((f) => this.grid?.clearHeaderFilter(f));
+  getFilterValue = input<(field: string) => string>((f) => this.grid?.getFilterValue(f) ?? '');
 }
