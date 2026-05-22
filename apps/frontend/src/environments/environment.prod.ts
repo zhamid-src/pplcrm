29661acb0ddf1695344fabe4107809e24917135b
Zee <zhamid@gmail.com>
2026-05-21 22:42:30 -0400

    chore: update google maps api key environment variable prefix to VITE_

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pplcrm.example.com',
-  googleMapsApiKey: import.meta.env['NX_GOOGLE_MAPS_API_KEY'] ?? '',
+  googleMapsApiKey: import.meta.env['VITE_GOOGLE_MAPS_API_KEY'] ?? '',
 };
