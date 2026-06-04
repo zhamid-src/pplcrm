d230a82e1d457767132af440057054f87e68bc0e
pplCRM <hello@pplcrm.com>
2026-06-03 21:31:28 -0400

    Changing the logo.

--- apps/frontend/src/app/auth/auth-layout.ts
+++ apps/frontend/src/app/auth/auth-layout.ts
@@ -10,8 +10,8 @@ import { Alerts } from '@uxcommon/components/alerts/alerts';
   template: `
     <div class="bg-image flex min-h-screen font-light" data-theme="light">
       <div class="card card-compact glass m-auto w-96 shadow-xl">
-        <div class="card-title mb-0 shadow-lg">
-          <img class="p-5" src="assets/logo.svg" />
+        <div class="card-title justify-center shadow-lg">
+          <img class="p-5" src="assets/logo.png" />
         </div>
         <pc-alerts />
         <div class="card-body">
