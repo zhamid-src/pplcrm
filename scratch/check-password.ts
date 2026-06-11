a618732ab321455bcdeef7ebbf7eecfec8db99c2
pplCRM <hello@pplcrm.com>
2026-06-11 10:27:45 -0400

    cleanup

--- scratch/check-password.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const hash = '$2b$10$t5uTrvGiUwK4SoHOjG3aiOtwDDwGHh0C7uf/80R7tdNBrQtPG0b.K';
-console.log('password123 matches:', bcrypt.compareSync('password123', hash));
-console.log('StrongPassword123! matches:', bcrypt.compareSync('StrongPassword123!', hash));
-console.log('pplcrm matches:', bcrypt.compareSync('pplcrm', hash));
-console.log('[REDACTED] matches:', bcrypt.compareSync('[REDACTED]', hash));
-console.log('password matches:', bcrypt.compareSync('password', hash));
-console.log('admin matches:', bcrypt.compareSync('admin', hash));
